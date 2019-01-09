4d449045e826b61830e52d7dc36ff98f668583a9
Jacob Hsu <jacob.hsu.tw@gmail.com>
2019-01-09 15:22:18 +0800

    vuejs show bootstrap modal

--- README.md
+++ README.md
@@ -6,9 +6,11 @@
 
 `npm i axios vue-axios --save`  
 `npm i bootstrap --save`  
-`npm i node-sass sass-loader --save`  
+`npm i node-sass sass-loader --save` 
+`npm i --save jquery popper.js` 
 
 Vue Router [Navigation Guards](https://router.vuejs.org/guide/advanced/navigation-guards.html)  
 
 [Bootstrap 4](https://getbootstrap.com/)  
-examples [Sign-in](https://getbootstrap.com/docs/4.2/examples/sign-in/) [Dashboard](https://getbootstrap.com/docs/4.2/examples/dashboard/)  
+examples [Sign-in](https://getbootstrap.com/docs/4.2/examples/sign-in/) [Dashboard](https://getbootstrap.com/docs/4.2/examples/dashboard/)    
+Documentation [Modal](https://getbootstrap.com/docs/4.2/components/modal/)  
--- eshop/src/components/pages/Products.vue
+++ eshop/src/components/pages/Products.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="text-right mt-4">
-            <button class="btn btn-primary">建立新的產品</button>
+            <button class="btn btn-primary" @click="openModel">建立新的產品</button>
         </div>
         <table class="table mt-4">
             <thead>
@@ -34,10 +34,33 @@
                 </tr>
             </tbody>
         </table>
+
+        <!-- Modal -->
+        <div class="modal fade" id="productModal" tabindex="-1" role="dialog" aria-labelledby="productModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="productModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
+
     </div>
 </template>
 
 <script>
+import $ from 'jquery';
 export default {
     data() {
         return {
@@ -52,6 +75,9 @@ export default {
                 console.log(response.data);
                 vm.products = response.data.products;
             }) 
+        },
+        openModel() {
+            $('#productModal').modal('show');
         }
     },
     created() {
